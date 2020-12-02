44d81b6637e721e43930a8b1b762605388a354b0
Aaron Lee <aaronlab.net@gmail.com>
2020-12-02 22:54:51 +0900

    Init DoublePriorityQueue

--- Sort/CombSort.playground/Contents.swift
+++ Sort/CombSort.playground/Contents.swift
@@ -1,47 +1,107 @@
+//import Foundation
+//
+//func solution(_ numbers:[Int]) {
+////    let answer = numbers.combSort().map { String($0) }.joined()
+//    let answer = numbers.combSort().combSort().customSort()
+//    print(answer)
+////    return answer.first == "0" ? "0" : answer
+//}
+//
+//extension Array where Self.Element == Int {
+//
+//    func combSort() -> Self {
+//        var copy = self
+//
+//        var gap = copy.count
+//        let shrink = 1.3
+//
+//        while gap > 1 {
+//            gap = Int(Double(gap) / shrink)
+//
+//            if gap < 1 {
+//                gap = 1
+//            }
+//
+//            var index = 0
+//
+//            while !(index + gap >= self.count) {
+//                if copy[index] > copy[index + gap] {
+//                    copy.swapAt(index, index + gap)
+//                }
+//                index += 1
+//            }
+//        }
+//
+//        print(copy)
+//        return copy
+//    }
+//
+//    func customSort() -> String {
+//        var temp = [String]()
+//
+//        for chunked in self.chunked(into: 2) {
+//            let max = chunked.max()!
+//            let min = chunked.min()!
+//            temp.append("\(max)\(min)")
+//        }
+//
+//        return temp.joined()
+//    }
+//
+//}
+//
+//extension Array {
+//    func chunked(into size: Int) -> [[Element]] {
+//        return stride(from: 0, to: count, by: size).map {
+//            Array(self[$0..<Swift.min($0 + size, count)])
+//        }
+//    }
+//}
+//
+
+
 import Foundation
 
-func solution(_ numbers:[Int]) {
-    let answer = numbers.map{String($0)}
-//        .sorted { $0 + $1 > $1 + $0 }
-//        .joined()
-    print(answer)
-    
-//    return answer.first == "0" ? "0" : answer
+func solution(_ numbers:[Int]) -> String {
+    // let answer: String = numbers.map{String($0)}.sorted { $0 + $1 > $1 + $0 }.joined()
+    let answer = numbers.customCombSort().map { String($0) }.joined()
+    return answer.first == "0" ? "0" : answer
 }
 
-func combSort() {
-    var input = [6, 10, 2]
-    var copy = input
+// 이상한 짓을 해볼까아아아
+
+extension Array where Self.Element == Int {
     
-    var answer = input.map { String($0) }
-    
-    var gap = input.count
-    let shrink = 1.3
-    
-    while gap > 1 {
-        gap = Int(Double(gap) / shrink)
-        if gap < 1 {
-            gap = 1
+    func customCombSort() -> Self {
+        var copy = self
+        
+        var gap = copy.count
+        let shrink = 1.3
+        
+        while gap > 1 {
+            gap = Int(Double(gap) / shrink)
+            
+            if gap < 1 {
+                gap = 1
+            }
+            
+            var index = 0
+            
+            while !(index + gap >= self.count) {
+                if Int("\(self[index])\(copy[index + gap])")! > Int("\(copy[index + gap])\(self[index])")! {
+                    copy.swapAt(index, index + gap)
+                }
+                index += 1
+            }
         }
         
-        var index = 0
-        while !(index + gap >= input.count) {
-            if input[index] > copy[index + gap]  {
-                copy.swapAt(index, index + gap)
-            }
-            index += 1
-        }
+        copy.reverse()
+        
+        return copy
     }
     
-    print(copy)
 }
 
-//print(solution([6, 10, 2]))
-//print(solution([3, 30, 34, 5, 9]))
-
-//solution([6, 10, 2])
-//solution([3, 30, 34, 5, 9])
-//solution([1000, 30, 34, 5, 9])
-
-combSort()
-//print(combSort())
+print(solution([6, 10, 2]))
+print(solution([3, 30, 34, 5, 9]))
+print(solution([303, 300, 34, 5, 9, 600, 500, 800, 900, 111, 222, 667, 445]))
