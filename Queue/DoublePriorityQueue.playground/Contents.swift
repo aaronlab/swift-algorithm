cbfa28a5353f90b0d0d5efb08b3a6e0b4cff44ed
Aaron Lee <aaronlab.net@gmail.com>
2020-12-02 22:57:00 +0900

    Init Solution Func

--- Queue/DoublePriorityQueue.playground/Contents.swift
+++ Queue/DoublePriorityQueue.playground/Contents.swift
@@ -1,3 +1,8 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+func solution(_ operations:[String]) -> [Int] {
+    return []
+}
+
+print(solution(["I 16", "D 1"])) // ---> [0, 0]
+print(solution(["I 7", "I 5", "I -5", "D -1"])) // ---> [7, 5]
