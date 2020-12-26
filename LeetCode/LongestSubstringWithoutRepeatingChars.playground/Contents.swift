721e7af22e5565af080d7ec36448eb1d1b9802b5
Aaron Lee <aaronlab.net@gmail.com>
2020-12-26 22:37:50 +0900

    Solution: Longest Substring

--- LeetCode/LongestSubstringWithoutRepeatingChars.playground/Contents.swift
+++ LeetCode/LongestSubstringWithoutRepeatingChars.playground/Contents.swift
@@ -2,6 +2,21 @@ import Foundation
 
 class Solution {
     func lengthOfLongestSubstring(_ s: String) -> Int {
-        
+        guard !s.isEmpty else { return 0 }
+        var length = 0
+        var chars = [Character]()
+        for c in s {
+            if let index = chars.firstIndex(of: c) {
+                length = max(length, chars.count)
+                chars.removeSubrange(0...index)
+            }
+            chars.append(c)
+        }
+        return max(length, chars.count)
     }
 }
+
+print(Solution().lengthOfLongestSubstring("abcabcbb")) // ==> 3
+print(Solution().lengthOfLongestSubstring("bbbbb")) // ==> 1
+print(Solution().lengthOfLongestSubstring("pwwkew")) // ==> 3
+print(Solution().lengthOfLongestSubstring("")) // ==> 0
