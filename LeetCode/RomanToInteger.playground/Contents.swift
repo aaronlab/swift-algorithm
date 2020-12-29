571583f08795a376dec6a43f2567de9141d9322d
Aaron Lee <aaronlab.net@gmail.com>
2020-12-29 22:56:30 +0900

    Init Contents

--- LeetCode/RomanToInteger.playground/Contents.swift
+++ LeetCode/RomanToInteger.playground/Contents.swift
@@ -2,6 +2,50 @@ import Foundation
 
 class Solution {
     func romanToInt(_ s: String) -> Int {
-        return 0
+        var answer = 0
+        let symbols = Array(s).map { Symbol(rawValue: $0) }
+        for symbol in symbols {
+            guard let symbol = symbol else { continue }
+            answer += symbol.value
+        }
+        return answer
+    }
+    
+    enum Symbol: Character {
+        case i = "I"
+        case v = "V"
+        case x = "X"
+        case l = "L"
+        case c = "C"
+        case d = "D"
+        case m = "M"
+        
+        var value: Int {
+            switch self {
+            case .i:
+                return 1
+            case .v:
+                return 5
+            case .x:
+                return 10
+            case .l:
+                return 50
+            case .c:
+                return 100
+            case .d:
+                return 500
+            case .m:
+                return 1000
+            }
+        }
+        
     }
 }
+
+let solution = Solution()
+
+print(solution.romanToInt("III")) // ==> 3
+print(solution.romanToInt("IV")) // ==> 4
+print(solution.romanToInt("IX")) // ==> 9
+print(solution.romanToInt("LVIII")) // ==> 58
+print(solution.romanToInt("MCMXCIV")) // ==> 1994
