400566abd3d1cb3400eb3be8ea60eff265a9964b
Aaron Lee <aaronlab.net@gmail.com>
2020-12-30 21:45:43 +0900

    Solution: LCP

--- LeetCode/LongestCommonPrefix.playground/Contents.swift
+++ LeetCode/LongestCommonPrefix.playground/Contents.swift
@@ -1,3 +1,20 @@
-import UIKit
+import Foundation
 
-var str = "Hello, playground"
+class Solution {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        if strs.count == 0 { return "" }
+        let shortest = strs.min { $0.count < $1.count }!
+        var prefix = shortest
+        for word in strs {
+            while !word.hasPrefix(prefix), prefix.count > 0 {
+                prefix.removeLast()
+            }
+            if prefix.count == 0 { return "" }
+        }
+        return prefix
+    }
+}
+
+print(Solution().longestCommonPrefix(["flower","flow","flight"])) // ==> "fl"
+print(Solution().longestCommonPrefix(["dog","racecar","car"])) // ==> ""
+print(Solution().longestCommonPrefix(["cir","car"])) // ==> "c"
