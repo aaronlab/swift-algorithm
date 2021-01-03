60fa45f4cd9dddcd956ae817ef84a436169fc150
Aaron Lee <aaronlab.net@gmail.com>
2021-01-03 15:31:58 +0900

    Single Number: Second Solution

--- LeetCode/SingleNumber.playground/Contents.swift
+++ LeetCode/SingleNumber.playground/Contents.swift
@@ -2,22 +2,7 @@ import Foundation
 
 class Solution {
     func singleNumber(_ nums: [Int]) -> Int {
-        if nums.count == 1 { return nums[0] }
-        
-        var dict = [Int: Int]()
-        
-        for i in 0..<nums.count {
-            guard dict[nums[i]] != nil
-            else {
-                dict[nums[i]] = 1
-                continue
-            }
-            dict[nums[i]]! += 1
-        }
-        
-        guard let key = dict.filter({ $0.value == 1 }).keys.first else { return 0 }
-        
-        return key
+        return nums.reduce(0, ^)
     }
 }
 
