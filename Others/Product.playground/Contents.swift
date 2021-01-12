0b0229d068b9ec641602589c05ee2c5805bb9b82
Aaron Lee <aaronlab.net@gmail.com>
2021-01-12 21:17:47 +0900

    Variation

--- Others/Product.playground/Contents.swift
+++ Others/Product.playground/Contents.swift
@@ -4,8 +4,11 @@ struct Option {
     let name: String
     let priority: Int
     let variations: [Variation]
+    let id: String
 }
 
 struct Variation {
-    
+    let value: String
+    let priority: Int
+    let id: String
 }
