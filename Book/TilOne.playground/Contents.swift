a8c9a2d18ae9ca006a5387f7367ed52574342460
Aaron Lee <aaronlab.net@gmail.com>
2021-01-29 23:07:26 +0900

    til 1

---
+++ Book/TilOne.playground/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+/*
+ Until 1
+ subtract
+ or
+ devide
+ 
+ how many times
+ */
+
+func solution(_ n: Int, _ k: Int) -> Int {
+    
+    var copy = n
+    var count = 0
+    
+    while copy != 1 {
+        
+        if copy % k == 0 {
+            copy /= k
+        } else {
+            copy -= 1
+        }
+        
+        count += 1
+        
+    }
+    
+    return count
+}
+
+print(solution(17, 4)) // => 3
+print(solution(25, 5)) // => 2
