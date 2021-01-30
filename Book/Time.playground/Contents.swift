f34a009f93f9da4e47978601e666b99d39ab03db
Aaron Lee <aaronlab.net@gmail.com>
2021-01-30 16:15:20 +0900

    init time

---
+++ Book/Time.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+func solution(_ n: Int) -> Int {
+    
+    for i in 1...24 {
+        // hrs
+        for j in 1...60 {
+            // mins
+            for k in 1...60 {
+                // secs
+            }
+        }
+    }
+    
+    return 0
+}
+
+print(solution(1))
