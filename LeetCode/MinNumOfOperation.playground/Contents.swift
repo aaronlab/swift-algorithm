448cac147bb656b8b9e9f20288f0216a817fa486
Aaron Lee <aaronlab.net@gmail.com>
2021-02-24 18:30:00 +0900

    init

---
+++ LeetCode/MinNumOfOperation.playground/Contents.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+class Solution {
+    func minOperations(_ boxes: String) -> [Int] {
+        var answer = [Int]()
+        
+        
+        
+        return answer
+    }
+}
+
+let solution = Solution()
+
+print(solution.minOperations("110"))
+print(solution.minOperations("001011"))
