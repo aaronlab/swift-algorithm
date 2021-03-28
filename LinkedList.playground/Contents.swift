6ae39c35c9d4550161ff1d7dac565c6d0a659005
Aaron Lee <aaronlab.net@gmail.com>
2021-03-28 22:03:01 +0900

    linked list structure

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -1,5 +1,18 @@
 import Foundation
 
+struct LinkedList<Value> {
+    
+    var head: Node<Value>?
+    var tail: Node<Value>?
+    
+    var isEmpty: Bool {
+        return head == nil
+    }
+    
+    init() {}
+    
+}
+
 class Node<Value> {
     
     var value: Value
@@ -24,11 +37,4 @@ extension Node: CustomStringConvertible {
     
 }
 
-let node1 = Node(value: 1)
-let node2 = Node(value: 2)
-let node3 = Node(value: 3)
-
-node1.next = node2
-node2.next = node3
-
-print(node1)
+let list = LinkedList<Int>()
