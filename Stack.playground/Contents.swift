014450f1c7130e100c4da13331078d5ff4cd0d9e
Aaron Lee <aaronlab.net@gmail.com>
2021-03-29 21:48:47 +0900

    implement push and pop

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -6,13 +6,21 @@ struct Stack<Element> {
     
     init() {}
     
+    mutating func push(_ element: Element) {
+        storage.append(element)
+    }
+    
+    mutating func pop() -> Element? {
+        return storage.popLast()
+    }
+    
 }
 
 extension Stack: CustomStringConvertible {
     
     var description: String {
         
-        let topDivider = "-----top-----"
+        let topDivider = "-----top-----\n"
         let bottomDivider = "\n-------------"
         
         let stackElements = storage.map { "\($0)" }.reversed().joined(separator: "\n")
@@ -22,3 +30,15 @@ extension Stack: CustomStringConvertible {
     }
     
 }
+
+var stack = Stack<Int>()
+
+stack.push(20)
+stack.push(10)
+stack.push(3)
+
+print(stack)
+
+stack.pop()
+
+print(stack)
