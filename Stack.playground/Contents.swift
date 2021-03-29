551b305cfeedb34a1b73a854ea431f21da6c48b5
Aaron Lee <aaronlab.net@gmail.com>
2021-03-29 21:43:17 +0900

    implement stack

---
+++ Stack.playground/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+struct Stack<Element> {
+    
+    private var storage: [Element] = []
+    
+    init() {}
+    
+}
+
+extension Stack: CustomStringConvertible {
+    
+    var description: String {
+        
+        let topDivider = "-----top-----"
+        let bottomDivider = "\n-------------"
+        
+        let stackElements = storage.map { "\($0)" }.reversed().joined(separator: "\n")
+        
+        return topDivider + stackElements + bottomDivider
+        
+    }
+    
+}
