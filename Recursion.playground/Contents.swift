8180759a6642d7e804792c365cbb69266b136295
Aaron Lee <aaronlab.net@gmail.com>
2021-03-30 21:20:35 +0900

    pow

--- Recursion.playground/Contents.swift
+++ Recursion.playground/Contents.swift
@@ -12,3 +12,15 @@ func factorial(_ number: Int) -> Int {
 
 let result = factorial(5)
 print(result)
+
+func power(_ number: Int, pow: Int) -> Int {
+    
+    if pow == 0 {
+        return 1
+    } else {
+        return number * power(number, pow: pow - 1)
+    }
+    
+}
+
+print(power(2, pow: 3))
